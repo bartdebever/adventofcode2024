f12e6d28e4f9922433581c41ab47b4f09e5c093f
Bart de Bever <bartdebever112@gmail.com>
2024-12-03 21:17:23 +0100

    Part 2 of day 2, recursive powershell hell

--- 2/part2.ps1
+++ 2/part2.ps1
@@ -1,8 +1,31 @@
 $SafeLevels = 0
 $Debug = $false
 
-foreach($line in Get-Content .\input.txt) {
-    $split = $line -split ' '
+function Test-Is-Safe {
+    param (
+        [string[]]$split,
+        [int]$Index,
+        [int]$Increment
+    )
+    
+    if (($Index + $Increment -gt $split.Length) -or (($Index + $Increment) -lt 0)) {
+        return $false
+    }
+
+    $list = [System.Collections.Generic.List[System.string]]$split
+    $list.RemoveAt($i + $Increment)
+    return Is-Line-Safe $list 1
+}
+
+function Is-Line-Safe {
+    param (
+        [string[]]$split,
+        [Parameter(Mandatory=$false)]
+        [int]$Iteration = 0
+    )
+    if ($Debug) {
+        Write-Host $split
+    }
     $decreasing = [int]($split[0]) -gt [int]($split[1])
     $Safe = $true
     for ($i = 0; $i -lt $split.Length - 1; $i++) {
@@ -33,20 +56,37 @@ foreach($line in Get-Content .\input.txt) {
             }
             $Safe = $false
         }
+
+        if ($Safe -eq $false) {
+            # Exit early if this was already nested
+            if ($Iteration -eq 1) {
+                return $false
+            }
+
+            return (Test-Is-Safe $split $i -1) -or  (Test-Is-Safe $split $i 1) -or  (Test-Is-Safe $split $i 0)
+        }
     }
 
     if ($Safe -eq $true) {
         if ($Debug) {
             Write-Host 'Safe'
         }
-
-        $SafeLevels++
     }
 
     if ($Debug) {
         Read-Host
     }
 
+    return $Safe
+
+
+}
+
+foreach($line in Get-Content .\input.txt) {
+    $split = $line -split ' '
+    if ((Is-Line-Safe $split) -eq $true) {
+        $SafeLevels++;
+    }
 }
 
 Write-Host $SafeLevels
